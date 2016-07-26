a66e5720449a8f5a48379c351f7493ba1f34c2b0
Elizabeth Carranza <elizabeth.m.carranza@gmail.com>
2016-07-26 00:05:43 -0700

    Angular-Animation

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var app = angular.module('myApp', ['ngMessages'])
+
+app.constant('VERSION', "6.4")
+app.run(function (VERSION, $rootScope) {
+    $rootScope.version = VERSION;
+})
+
+.controller("myCtrl", function ($scope) {
+    $scope.data = {};
+
+    $scope.gender = {};
+
+    $scope.inputHide = false;
+
+    $scope.submit = function () {
+        $scope.inputHide = true;
+    }
+    $scope.reset = function () {
+        $scope.inputHide = false;
+        $scope.data = {};
+        location.reload();
+    }
+});
